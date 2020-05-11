414ecfc3a0193b372695b58a96affb095f44e95b
ehdgh <ehdghksahd@naver.com>
2020-05-12 00:01:54 +0900

    SpawnManagerTest

--- Assets/Scripts/AnchorManager.cs
+++ Assets/Scripts/AnchorManager.cs
@@ -6,21 +6,25 @@ using UnityEngine;
 
 public class AnchorManager : MonoBehaviour
 {
+    List<Restaurant> restaurants = new List<Restaurant>();
+    List<Vector3> vectors = new List<Vector3>();
+    List<Pose> poses = new List<Pose>();
+    List<Anchor> anchors = new List<Anchor>();
+    List<GameObject> gameObjects = new List<GameObject>();
+    Dictionary<string, string> dic = new Dictionary<string, string>();
     public GameObject anchoredPrefab;
-    Anchor anchor;
-    Vector3 vector3;
+
+    TextMesh[] textMeshs;
 
     Vector3 lastAnchoredPosition;
     Quaternion lastAnchoredRotation;
 
     float degreesLongitudeInMetersAtEquator;
-  
+
     private void Start()
     {
         StartCoroutine(DelayMethod(3.5f, () =>
         {
-            float latitude = 36.137266f;
-            float longitude = 128.3965608f;
             // Conversion factors
             float degreesLatitudeInMeters = 111132;
             degreesLongitudeInMetersAtEquator = 111319.9f;
@@ -28,39 +32,85 @@ public class AnchorManager : MonoBehaviour
             // Real GPS Position - This will be the world origin.
             var gpsLat = GPSManager_NoCompass.Instance.latitude;
             var gpsLon = GPSManager_NoCompass.Instance.longitude;
+            dic.Add("url", "restaurants/near");
+            dic.Add("lat", gpsLat.ToString());
+            dic.Add("lon", gpsLon.ToString());
+            dic.Add("radius", "200");
 
-            Debug.Log(gpsLat);
-            Debug.Log(gpsLon);
+            //restaurants = onclick();
 
             // GPS position converted into unity coordinates
-            var latOffset = (latitude - gpsLat) * degreesLatitudeInMeters;
-            var lonOffset = (longitude - gpsLon) * GetLongitudeDegreeDistance(latitude);
+            foreach (Restaurant restaurant in restaurants)
+            {
+                var latOffset = (restaurant.getLat() - gpsLat) * degreesLatitudeInMeters;
+                var lonOffset = (restaurant.getLon() - gpsLon) * GetLongitudeDegreeDistance(restaurant.getLat());
 
-            vector3 = new Vector3(latOffset, 0, latOffset);
-            Debug.Log(vector3);
-            Debug.Log(transform.position);
+                Vector3 vector3 = new Vector3(latOffset, 0, latOffset);
+
+                vectors.Add(vector3);
+            }
+
         }));
- 
+
     }
 
     void Update()
     {       // Real world position of object. Need to update with something near your own location.
         if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
-            Pose pose = new Pose(vector3, transform.rotation);
+            for (int i = 0; i < vectors.Count; i++)
+            {
+                Pose pose = new Pose(vectors[i], transform.rotation);
 
-            anchor = Session.CreateAnchor(pose);
+                poses.Add(pose);
 
-            var gameObject = Instantiate(anchoredPrefab, anchor.transform.position, anchoredPrefab.transform.rotation, anchor.transform);
-            lastAnchoredPosition = anchor.transform.position;
+                anchors.Add(Session.CreateAnchor(pose));
+
+                var gameObject = Instantiate(anchoredPrefab, anchors[i].transform.position, anchoredPrefab.transform.rotation, anchors[i].transform);
+
+                textMeshs = gameObject.GetComponentsInChildren<TextMesh>();
+
+
+                textMeshs[0].text = restaurants[i].score;
+                textMeshs[1].text = restaurants[i].name;
+                textMeshs[2].text = restaurants[i].brief; 
+
+            }
+
+            foreach (Anchor anchor in anchors)
+            {
+                gameObjects.Add(gameObject);
+            }
+
+
+
+
         }
-        if (anchor == null) return;
-        if (lastAnchoredPosition != anchor.transform.position)
+        if (Input.touchCount > 1 && gameObjects.Count != 0)
         {
-            Debug.Log(Vector3.Distance(anchor.transform.position, lastAnchoredPosition));
-            lastAnchoredPosition = anchor.transform.position;
+            Vector2 pos = Input.GetTouch(Input.touchCount).position;
+            Vector3 theTouch = new Vector3(pos.x, pos.y, 0.0f);    // 변환 안하고 바로 Vector3로 받아도 되겠지.
+
+            Ray ray = Camera.main.ScreenPointToRay(theTouch);    // 터치한 좌표 레이로 바꾸엉
+
+            RaycastHit hit;    // 정보 저장할 구조체 만들고
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))    // 레이저를 끝까지 쏴블자. 충돌 한넘이 있으면 return true다.
+
+            {
+
+                if (Input.GetTouch(0).phase == TouchPhase.Began)    // 딱 처음 터치 할때 발생한다
+
+                {
+
+
+
+                }
+
+            }
         }
-        if(Session.Status != SessionStatus.Tracking)
+
+        if (Session.Status != SessionStatus.Tracking)
         {
             return;
         }
@@ -73,5 +123,9 @@ public class AnchorManager : MonoBehaviour
     {
         yield return new WaitForSeconds(waitTime);
         action();
+    }
+    public void SetObject()
+    {
+
     }
 }

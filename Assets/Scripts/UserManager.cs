c621dee0e5a2549b51737ad5f0a4f2861dcd3a26
KangWoon <river951@naver.com>
2020-05-18 03:13:31 +0900

    로그인 및 회원정보 조회, 수정 완료 자동 로그인기능 포함

--- Assets/Scripts/UserManager.cs
+++ Assets/Scripts/UserManager.cs
@@ -17,30 +17,57 @@ public class UserManager : MonoBehaviour
     public Text nameContent;
     public Text ageContent;
     public Text sexContent;
+
+    public Text UmailContent;
+    public InputField CurrentPWField;
+    public InputField ChangePWField;
+    public InputField CheckChangePWField;
+
+    public GameObject UpdateInfo;
+    public GameObject UnMatchCurrentError;
+    public GameObject UnMatchChangeError;
+
     // Start is called before the first frame update
     void Start()
     {
-        var pairs = new Dictionary<string, string>();
-        pairs["url"] = Login.userId + "/profile";
-        pairs["password"] = Login.userPw;
-        pairs["method"] = "GET";
+        getUserInfo();
+        UpdateInfo.SetActive(false);
+        UnMatchCurrentError.SetActive(false);
+        UnMatchChangeError.SetActive(false);
 
-        Debug.Log(pairs["url"] + pairs["password"]);
+        Debug.Log("세션"+PlayerPrefs.GetString("ID"));
 
-        IEnumerator enumerator = handleUnserInfo(pairs);
-
-        StartCoroutine(enumerator);
-      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    public void getUserInfo()
+    {
+        var pairs = new Dictionary<string, string>();
+        pairs["url"] = Login.userId + "/profile";
+        pairs["password"] = Login.userPw;
+        //pairs["url"] = "nsh722/profile"; //실제 id넣어야함
+        pairs["method"] = "GET";
+
+        Debug.Log(pairs["url"] + pairs["password"]);
+
+        IEnumerator enumerator = handleUserInfo(pairs);
+
+        StartCoroutine(enumerator);
     }
 
 
-    private IEnumerator handleUnserInfo(Dictionary<string, string> pairs)
+    private IEnumerator handleUserInfo(Dictionary<string, string> pairs)
     {
         Debug.Log("이벤트 핸들 진입");
         flagWakeUp = false;
@@ -52,7 +79,7 @@ public class UserManager : MonoBehaviour
 
         Debug.Log("핸들러 종료");
 
-        mailContent.text = Login.userId;
+        mailContent.text = Login.userId; // 실제 id를 넣어야함
         nameContent.text = check["user"]["name"].ToString();
         ageContent.text = check["user"]["age"].ToString();
         if(check["user"]["sex"].ToString() == "f")
@@ -64,11 +91,102 @@ public class UserManager : MonoBehaviour
             sexContent.text = "남성";
         }
         
+    }
 
+    public void clickUpdateInfo()
+    {
+        UmailContent.text = Login.userId; // 실제 id와 비교 해야함
+        UpdateInfo.SetActive(true);
+    }
+
+    public void clickCompleteUpdate()
+    {
+        if (CurrentPWField.text == Login.userPw) // 실제 비밀번호와 비교 해야함
+        {
+            if (ChangePWField.text == CheckChangePWField.text)
+            {
+
+                Login.userPw = ChangePWField.text;
+                if(PlayerPrefs.HasKey("PW"))
+                {
+                    PlayerPrefs.SetString("PW", ChangePWField.text);
+                }
+
+                var pairs = new Dictionary<string, string>();
+                //pairs["url"] = Login.userId + "/profile";
+                //pairs["password"] = Login.userPw;
+                pairs["url"] = UmailContent.text + "/profile";
+                pairs["new_password"] = ChangePWField.text;
+                //pairs["password"] = CurrentPWField.text;
+                pairs["method"] = "PUT";
+
+
+                IEnumerator enumerator = handleUpdateInfo(pairs);
+
+                StartCoroutine(enumerator);
+
+            }
+            else
+            {
+                UnMatchChangeError.SetActive(true);
+                ChangePWField.text = "";
+                CheckChangePWField.text = "";
+            }
+        }
+        else
+        {
+            Debug.Log("현재 비밀번호 틀림");
+            UnMatchCurrentError.SetActive(true);
+            CurrentPWField.text = "";
+        }
+    }
+
+    private IEnumerator handleUpdateInfo(Dictionary<string, string> pairs)
+    {
+
+        Debug.Log("서버로 변경내용 전송");
+        flagWakeUp = false;
+        eventHandler.onClick(this, serverManager.SendRequest(pairs), EventHandler.HandlingType.Restaurants);
+        Debug.Log("핸들러 온클릭");
+        while (!flagWakeUp)
+            yield return new WaitForSeconds(1.0f);
+        var check = eventHandler.result as JsonData;
+
+        Debug.Log(check["code"].ToString());
+        if (check["code"].ToString() == "success")
+        {
+            UpdateInfo.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("뭔가 오류가 났음");
+        }
 
 
     }
-public void WakeUp()
+
+    public void CurrentOk()
+    {
+        UnMatchCurrentError.SetActive(false);
+    }
+    public void ChangeOk()
+    {
+        UnMatchChangeError.SetActive(false);
+    }
+
+    public void Logout()
+    {
+        if(PlayerPrefs.HasKey("ID"))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+        Login.userId = "";
+        Login.userPw = "";
+        SceneManager.LoadScene("Login");
+    }
+
+
+    public void WakeUp()
     {
         flagWakeUp = true;
     }

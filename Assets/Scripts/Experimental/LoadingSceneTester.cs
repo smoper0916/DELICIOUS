9ec5f82fedd4dd73aa76a0a9aaaee4ad0355e6af
smoper0916 <smoper0916@gmail.com>
2020-05-18 03:16:04 +0900

    Add Sample Test

--- Assets/Scripts/Experimental/LoadingSceneTester.cs
+++ Assets/Scripts/Experimental/LoadingSceneTester.cs
@@ -19,13 +19,19 @@ public class LoadingSceneTester : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Input.gyro.enabled = true;
+        StartCoroutine(OnGyro);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public IEnumerator OnGyro()
+    {
+        StartCoroutine(waitThenCallback(1, () => { Debug.Log(Input.gyro.attitude+","+Input.gyro.gravity+","+Input.gyro.) }));
     }
 
     public void OnClick()

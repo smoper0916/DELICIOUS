28b5bacf97111bea24ff703d054e32ea042fc4c6
smoper0916 <smoper0916@gmail.com>
2020-06-09 02:10:07 +0900

    Fix Compass Mover BUg

--- Assets/Scripts/CompassMover.cs
+++ Assets/Scripts/CompassMover.cs
@@ -13,19 +13,70 @@ public class CompassMover : MonoBehaviour
     public bool isDone = false;
     public bool isGoodDegree = false;
 
+
+    public float reading;
+    public Queue<float> data;
+    private float[] dataList;
+    public int maxData = 5;
+    public int count;
+    public float sum, average;
+
+    public float dampening = 0.1f;
+    public float rotationToNorth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        data = new Queue<float>();
+        dataList = new float[maxData];
+        rotationToNorth = GPSManager.Instance.heading;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isDone)
+        if (!isDone)
         {
-            var heading = (int)(GPSManager.Instance.heading);
+            count = data.Count;
+
+            if (data.Count > 0)
+            {
+                data.CopyTo(dataList, 0);
+            }
+
+            if (data.Count == maxData)
+            {
+                for (int i = 0; i < data.Count; i++)
+                {
+                    sum += dataList[i];
+                }
+                if (Mathf.Abs(dataList[maxData - 1]) > 0)
+                {
+                    average = sum / maxData;
+                    sum = 0;
+                    data.Clear();
+                    dataList = new float[maxData];
+
+                }
+            }
+
+            if (data.Count >= maxData)
+            {
+                data.Dequeue();
+            }
+
+            reading = Mathf.Round(GPSManager.Instance.heading);
+            data.Enqueue(reading);
+
+                /*
+                rotationToNorth = average / 180;
+                rotationToNorth = (Mathf.Round(rotationToNorth * 10)) / 10;
+                rotationToNorth = rotationToNorth - 1;
+                */
+
+            rotationToNorth = Mathf.LerpAngle(rotationToNorth, GPSManager.Instance.heading, 0.1f);
             // 나침반에 대해 실시간 변경
+            var heading = (int)(rotationToNorth);
             image.transform.rotation = Quaternion.Euler(0, 0, heading);
             angleText.text = heading.ToString();
 
@@ -46,6 +97,7 @@ public class CompassMover : MonoBehaviour
             {
                 guideText.text = "0도까지 회전해주세요.";
             }
+            
         }
         else
         {
